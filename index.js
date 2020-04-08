7429cfef0256f5911f9a93e33624c8666da438dc
ALEX-THE-CHIP <63328287+ALEX-THE-CHIP@users.noreply.github.com>
2020-04-08 12:19:36 +1000

    initial version

---
+++ index.js
@@ -0,0 +1,121 @@
+var number = 5; // in-line comment
+
+/* this is a
+dsfghjkkkkkkkjhg
+gh
+yujkj
+multi-line comment */
+
+number = 9;
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Beau";
+
+myName = "Alex";
+
+let ourName = "1";
+
+const pi = 3.14152965358979323869;
+
+var a;
+var b = 2;
+console.log(a);
+a = 7;
+
+b = a;
+
+console.log(a);
+
+var a = 9;
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c= "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + "String!";
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assingnments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+var sum = 10 + 10;
+console.log(sum)
+
+var difference = 45 - 33;
+console.log(difference)
+
+var product = 20 * 20;
+console.log(product)
+
+var quotient = 66 / 33;
+console.log(quotient)
+
+var myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+
+myVar++;
+
+var myVar = 11;
+
+// Only change code below this line
+myVar = myVar - 1;
+
+myVar--;
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 0.009;
+
+var product = 2.0 * 2.5;
+console.log(product)
+
+var quotient = 6.6 / 3.3;
+console.log(quotient)
+
+// Only change code below this line
+
+var remainder;
+remainder = 11 % 3;
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a = a + 12;
+a += 12;
+b = 9 + b;
+b += 9;
+c = c + 7;
+c += 7;
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a = a - 6;
+a -= 6;
+b = b - 15;
+b -= 15;
+c = c - 1;
+c -= 1;
